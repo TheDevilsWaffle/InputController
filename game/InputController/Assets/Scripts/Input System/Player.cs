c32460bc21f086db7c3ebb422f40513dcb2d794c
TheDevilsWaffle <TheDevilsWaffle@gmail.com>
2018-01-16 19:15:25 -0800

    wip central input controller, mostly just skeleton layout

---
+++ game/InputController/Assets/Scripts/Input System/Player.cs
@@ -0,0 +1,49 @@
+﻿﻿///////////////////////////////////////////////////////////////////////////////////////////////////
+//AUTHOR — Travis Moore
+//SCRIPT — Player.cs
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma warning disable 0169
+#pragma warning disable 0649
+#pragma warning disable 0108
+#pragma warning disable 0414
+
+using UnityEngine;
+//using UnityEngine.UI;
+using System.Collections;
+//using System.Collections.Generic;
+
+#region ENUMS
+//public enum EnumStatus
+//{
+//	
+//};
+#endregion
+
+#region EVENTS
+//public class EVENT_EXAMPLE : GameEvent
+//{
+//    public EVENT_EXAMPLE() { }
+//}
+#endregion
+
+public class Player : MonoBehaviour
+{
+    #region FIELDS
+    [Header("Player Number")]
+    [SerializeField]
+    int playerNumber;
+    #endregion
+
+    #region INITIALIZATION
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>
+    /// OnValidate
+    /// </summary>
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    public Player(int _playerNumber)
+    {
+        playerNumber = _playerNumber;
+    }
+    #endregion
+}
